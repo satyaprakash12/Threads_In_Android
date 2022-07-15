c3c0ff3b76d71ae1b1480f3d30fc62e33c58aee0
Satya Prakash <satyaprakas5555@gmail.com>
2022-07-15 03:46:49 -0700

    Initial commit

---
+++ app/src/main/java/satyaprakash/Threads_In_Android/MainActivity.java
@@ -0,0 +1,14 @@
+package satyaprakash.Threads_In_Android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
